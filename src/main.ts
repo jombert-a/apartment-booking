25dd41a7b529799e88879624197213cbddb0b3bc
Akhiyarov Robert <nonrobertsbox@gmail.com>
2022-03-27 11:13:30 +0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./modules/app/app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+    .setTitle('Apartment Booking')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('longliverob')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
 	await app.listen(3000);
 }
 bootstrap();
